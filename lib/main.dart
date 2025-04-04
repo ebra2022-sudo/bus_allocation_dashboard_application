9444765de95ee325c197de5aaf0b7e4b66376ca1
ebra2022-sudo <ebrahimmuhammed479@gmail.com>
2025-04-04 18:04:50 +0300

    add Map about screen

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
         '/dashboard': (context) => const DashboardScreen(),
         '/settings': (context) => const SettingsScreen(),
         '/map-options': (context) => const MapOptionsScreen(),
+        '/about': (context) => const AboutScreen()
 
       },
     );
@@ -385,6 +386,28 @@ class MapOptionsScreen extends StatelessWidget {
       body: const Center(
         child: Text(
           'Map Options Screen',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
+
+// About Screen
+class AboutScreen extends StatelessWidget {
+  const AboutScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('About'),
+        backgroundColor: Colors.green,
+        leading: IconButton(onPressed: () => {Navigator.pop(context)}, icon: Icon(Icons.arrow_back)),
+      ),
+      body: const Center(
+        child: Text(
+          'About Screen',
           style: TextStyle(fontSize: 24),
         ),
       ),
